a0cbbc143e25a28d7d92c32bbeb560620c22c09a
gg161087 <gg161087@gmail.com>
2024-09-18 23:16:05 -0300

    Clase del 17/09/2024

--- Clase 17-09-2024/hola_mundo.py
+++ Clase 17-09-2024/hola_mundo.py
@@ -1,1 +1,14 @@
-print('Hola mundo')
+#comentario simple
+'''
+ comentario multilinea
+'''
+# variables
+# operador de asignacion (=)
+# tipo de datos simples
+
+nombre = 'Gonza' # print(type(saludo)) String
+edad = 37        # print(type(edad)) int
+peso = 92.50     # print(type(peso)) float
+altura = 1.84    # print(type(altura)) float
+ejercita = True  # print(type(ejercita)) boolean
+imc = peso / (altura*altura)  # print(imc) 27.321597353497165
